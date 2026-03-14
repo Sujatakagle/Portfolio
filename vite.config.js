6883a93e2fbfbf4ea18a7be0a0eaac7a04edd4e7
Sujatakagle <kaglesujata@gmail.com>
2026-03-14 15:31:29 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: `/${env.VITE_REPO_NAME}/`,
     plugins: [react(), tailwindcss()],
     server: {
       open: true,
